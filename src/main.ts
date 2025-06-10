a090dccbe2221fdc713e5fae94a60adb4674e08a
Ruslan Semak <ruslan.semack1501@gmail.com>
2025-06-10 20:56:46 +0300

    feat: Миграция с fastify на express

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,29 @@
 import { ShutdownSignal } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 import { APP_NAME, APP_VERSION } from './app.constants';
 import { AppModule } from './app.module';
 import { bootstrapPipes, bootstrapSwagger } from './bootstrap';
-import { appConfig } from './config';
-import { ExceptionFilter } from './filters';
+import { GlobalExceptionFilter } from './filters/globalException.filter';
+import { JsonApiSerializerInterceptor } from './interceptors/JsonApiSerializer.interceptor';
 import { DEVELOPMENT_STRATEGY, PinoService, PRODUCTION_STRATEGY } from './logger';
 
 async function bootstrap() {
   const pinoStrategy = process.env.NODE_ENV === 'production' ? PRODUCTION_STRATEGY : DEVELOPMENT_STRATEGY;
   const logger = new PinoService(pinoStrategy);
 
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter(), { logger });
+  const app = await NestFactory.create(AppModule, {
+    logger,
+    snapshot: true,
+  });
 
-  app.useGlobalFilters(new ExceptionFilter());
+  app.useGlobalInterceptors(new JsonApiSerializerInterceptor());
+  app.useGlobalFilters(new GlobalExceptionFilter());
   app.enableShutdownHooks([ShutdownSignal.SIGINT, ShutdownSignal.SIGTERM]);
 
   bootstrapSwagger(app);
   bootstrapPipes(app);
 
-  await app.listen({ port: appConfig.port, host: '127.0.0.1' });
+  await app.listen(2000);
 
   const context = 'Bootstrap';
   logger.log(`Listening on ${JSON.stringify(app.getHttpServer().address())}`, context);
